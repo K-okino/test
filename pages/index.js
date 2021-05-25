7011bb0b841a5bfdb1f1e243eaa60df651735709
“hirata <y_hirata@sbs-infosys.co.jp>
2021-05-25 11:29:23 +0900

    wao

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ export default function Home() {
   return (<>
     <p>藤澤の変更</p>
 <<<<<<< HEAD
-    <p>私は何も変更していない！信じてくれ！!</p>
   </>)
 >>>>>>> 1b478ef0185226fa829c45c42256057a55b0df8e
 }
@@ -11,6 +10,7 @@ export default function Home() {
     <p>榮田の変更</p>
     <p>古庄の変更</p> 
     <p>鹿間の変更</p>
+    <p>私は何も変更していない！信じてくれ！!</p>
   </>)
 }
 >>>>>>> 32ac24f6db3e1e66c1e91208d2ab988ce1457758
